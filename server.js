85e9b2ca38d78d478110b9c5ba53d1c63842be63
Tim Remington <Tim.Remington29@gmail.com>
2019-07-22 17:00:45 -0600

    added protected route

--- server.js
+++ server.js
@@ -3,15 +3,37 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
 
 const PORT = process.env.PORT || 4000
-
 const app = express()
+
+// Set up Auth0 configuration
+const authConfig = {
+  domain: "dev-8wpob8rd.auth0.com",
+  audience: "http://localhost:4000"
+}
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from YOUR_DOMAIN
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
+});
 
 // Middleware
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
+
 
 // Sequelize models
 const db = require('./models')
@@ -92,6 +114,15 @@ app.post('/api/checkout', async (req, res, next) => {
   catch (error) {
     next(error)
   }
+})
+
+// Define an endpoint that must be called with an access token
+app.get("/api/external", checkJwt, (req, res) => {
+
+  res.json({
+    msg: "Your Access Token was successfully validated!"
+  })
+
 })
 
 // Error Handling
